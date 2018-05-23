ef822ca54a44ac9ff327d77d8052bb23b55c2123
jmaldonadoa <jmaldonadoa@intergrupo.com>
2018-05-23 07:23:17 -0500

    Add project files.

---
+++ CameraAndroid/BitmapHelpers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CameraAndroid
+{
+    public static class BitmapHelpers
+    {
+        public static Bitmap LoadAndResizeBitmap(this string fileName, int width, int height)
+        {
+            BitmapFactory.Options options = new BitmapFactory.Options
+            {
+                InPurgeable = true,
+                InJustDecodeBounds = true
+            };
+            BitmapFactory.DecodeFile(fileName, options);
+            int outHeight = options.OutHeight;
+            int outWidth = options.OutWidth;
+            int inSampleSize = 1;
+            if (outHeight > height || outWidth > width)
+            {
+                inSampleSize = outWidth > outHeight ?
+                    outHeight / height :
+                    outWidth / width;
+            }
+            options.InSampleSize = inSampleSize;
+            options.InJustDecodeBounds = false;
+            Bitmap resizedBitmap = BitmapFactory.DecodeFile(fileName, options);
+            return resizedBitmap;
+        }
+    }
+}
---
+++ CameraAndroid/MainActivity.cs
@@ -0,0 +1,94 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Graphics;
+using Enviroment = Android.OS.Environment;
+using Uri = Android.Net.Uri;
+using System;
+using System.Collections.Generic;
+using Android.Content;
+using Android.Content.PM;
+using Android.Provider;
+using Java.IO;
+
+namespace CameraAndroid
+{
+    public static class App
+    {
+        public static File _file;
+        public static File _dir;
+        public static Bitmap bitmap;
+    }
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        private ImageView _imageView;
+        protected override void OnCreate(Bundle bundle)
+        {
+            StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+            StrictMode.SetVmPolicy(builder.Build());
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource  
+            SetContentView(Resource.Layout.activity_main);
+            if (IsThereAnAppToTakePictures())
+            {
+                CreateDirectoryForPictures();
+                Button button = FindViewById<Button>(Resource.Id.myButton);
+                _imageView = FindViewById<ImageView>(Resource.Id.imageView1);
+                button.Click += TakeAPicture;
+            }
+        }
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            Intent mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
+            Uri contentUri = Uri.FromFile(App._file);
+            mediaScanIntent.SetData(contentUri);
+            SendBroadcast(mediaScanIntent);
+            int height = Resources.DisplayMetrics.HeightPixels;
+            int width = _imageView.Height;
+            App.bitmap = App._file.Path.LoadAndResizeBitmap(width, height);
+            _imageView.SetImageURI(contentUri);
+            //using (Bitmap bitmap = App._file.Path.LoadAndResizeBitmap(width, height))
+            //{
+            //    //View ImageView  
+            //    //_imageView.RecycleBitmap();
+            //    _imageView.SetImageBitmap(bitmap);
+            //    //_imageView.SetImageURI(contentUri);
+            //    //Upload Image in Database  
+            //}
+            //if (App.bitmap != null)
+            //{
+            //    _imageView.SetImageBitmap(App.bitmap);
+            //    App.bitmap = null;
+            //}
+            GC.Collect();
+        }
+        private void CreateDirectoryForPictures()
+        {
+            App._dir = new File(
+                Android.OS.Environment.GetExternalStoragePublicDirectory(
+                    Android.OS.Environment.DirectoryPictures), "CameraAndroid");
+            if (!App._dir.Exists())
+            {
+                App._dir.Mkdirs();
+            }
+        }
+        private bool IsThereAnAppToTakePictures()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            IList<ResolveInfo> availableActivities =
+                PackageManager.QueryIntentActivities(intent, PackageInfoFlags.MatchDefaultOnly);
+            return availableActivities != null && availableActivities.Count > 0;
+        }
+        private void TakeAPicture(object sender, EventArgs eventArgs)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            App._file = new File(App._dir, String.Format("myPhoto_.jpg", Guid.NewGuid()));
+            intent.PutExtra(MediaStore.ExtraOutput, Uri.FromFile(App._file));
+            StartActivityForResult(intent, 0);
+        }
+    }
+}
+
